01562b6c7f5c423b2e9cc867d3c85c3ac91bf1a6
Justin Beaty <justin.beaty@gmail.com>
2023-09-28 05:51:22 -0700

    domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://justinbeaty.github.io',
+  base: '/rjp',
+});
