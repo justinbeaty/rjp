5067407926427d795aecff8d120e918d67dc0086
Justin Beaty <justin.beaty@gmail.com>
2023-10-02 16:05:31 -0700

    w

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,18 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.robertpotter.net',
-  integrations: [tailwind()],
+  integrations: [tailwind(), mdx()],
   vite: {
     resolve: {
-      alias: [
-        {
-          find: "@",
-          replacement: "src",
-        },
-      ],
-    },
-  },
+      alias: [{
+        find: "@",
+        replacement: "src"
+      }]
+    }
+  }
 });
